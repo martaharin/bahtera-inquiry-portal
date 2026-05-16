3dd7d52d3ecc3c63baff2924f70f456fd9e20903
martaharin <oliviamartah@gmail.com>
2026-05-16 11:46:31 +0700

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,24 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*", // Match all API routes
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Replace '*' with specific origin if needed
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
